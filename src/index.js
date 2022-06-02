c74e06739bb3708848f8fc471856b6fdeac1fcc1
valentinesamuel <valentinesamuel2580@gmail.com>
2022-06-02 12:35:10 +0100

    BUG: Tried to downgrade framer motion so that chakraUI but failed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    
+    <App />
+    
+    , rootElement);
